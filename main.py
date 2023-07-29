e824f5f5cf92cff6dc14add4c10929b8098e3fe5
naelitoh <nael.pontes.m@gmail.com>
2023-07-29 01:28:59 -0300

    testing different settings

--- main.py
+++ main.py
@@ -58,16 +58,14 @@ last_price = None
 interval = 5
 token = TELEBOT_TOKEN
 chatID = MY_CHAT_ID
-url = "https://www.pichau.com.br/gabinete-gamer-pichau-hx350-mid-tower-lateral-de-vidro-com-4-fans-preto-pg-hx35-bl01"
-# "https://www.kabum.com.br/produto/164854/placa-de-video-rtx-3060-asus-dual-o12g-v2-nvidia-geforce-12gb-gddr6-lhr-dlss-ray-tracing-dual-rtx3060-o12g-v2"
+url = "https://www.kabum.com.br/produto/164854/placa-de-video-rtx-3060-asus-dual-o12g-v2-nvidia-geforce-12gb-gddr6-lhr-dlss-ray-tracing-dual-rtx3060-o12g-v2"
 
 # Simulating the browser to gain access to the domain
 headers = {
-    # Mozilla user agent
-    "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0'
-
     # Chrome user agent
-    # 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+    # Mozilla user agent
+    # "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0'
 }
 
 # Calculate when next hour's minute is equal to :59
@@ -126,7 +124,7 @@ def routine():
         html = bs4.BeautifulSoup(req.content, 'html.parser')
 
         # Argument must be class_, because class is a reserved word in Python
-        price_element = html.find(class_ = "jss267")
+        price_element = html.find(class_ = "finalPrice")
 
         # Note: This block of code seems to be skipped in the GitHub Action... Gotta find out why
         if price_element is not None:

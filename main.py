dd6894aee6e9110de3f642c15561842e624f44ea
naelitoh <nael.pontes.m@gmail.com>
2023-07-29 00:17:29 -0300

    new features added for debug

--- main.py
+++ main.py
@@ -62,7 +62,11 @@ url = "https://www.kabum.com.br/produto/164854/placa-de-video-rtx-3060-asus-dual
 
 # Simulating the browser to gain access to the domain
 headers = {
-    "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+    # Mozilla user agent
+    "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0'
+
+    # Chrome user agent
+    # 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
 }
 
 # Calculate when next hour's minute is equal to :59
